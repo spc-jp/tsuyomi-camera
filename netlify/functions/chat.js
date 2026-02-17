5812e0e688683b3aa2abc844928a23a1751fce7d
YoshiBon <160085473+spc-jp@users.noreply.github.com>
2026-02-18 04:53:26 +0900

    Implement chat function handler with CORS headers

--- netlify/functions/chat.js
+++ netlify/functions/chat.js
@@ -1,1 +1,12 @@
-
+export async function handler(event) {
+return {
+statusCode: 200,
+headers: {
+"Access-Control-Allow-Origin": "*",
+"Access-Control-Allow-Headers": "Content-Type"
+},
+body: JSON.stringify({
+message: "chat function is alive"
+})
+};
+}
